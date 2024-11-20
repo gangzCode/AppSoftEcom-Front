a124b8aa41fc783e847636677403af259357a74d
Brinthan <brinthsega@gmail.com>
2024-11-21 03:33:31 +0530

    best selling products mapping

--- src/components/bestSeller.js
+++ src/components/bestSeller.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Box, Typography, IconButton } from "@mui/material";
 import { useSwipeable } from "react-swipeable";
 import {
@@ -9,73 +9,89 @@ import {
   Layers,
 } from "@mui/icons-material";
 import { Link } from "react-router-dom";
+import { getBestSellingProducts } from "../services/apiCalls";
 
 const BestSeller = () => {
   const scrollContainerRef = useRef(null);
   const [hoveredProductId, setHoveredProductId] = useState(null);
+  const [products, setproducts] = useState("");
 
-  const products = [
-    {
-      id: 1,
-      image:
-        "https://dt-elektrix.myshopify.com/cdn/shop/products/Electro07.jpg?v=1666157970&width=360",
-      hoverImage: "https://placehold.co/360x340?text=Hover+Image+1",
-      description: "Product 1",
-      price: "$19.99",
-    },
-    {
-      id: 2,
-      image: "https://placehold.co/360x340",
-      hoverImage: "https://placehold.co/360x340?text=Hover+Image+2",
-      description: "Product 2",
-      price: "$29.99",
-    },
-    {
-      id: 3,
-      image: "https://placehold.co/360x340",
-      hoverImage: "https://placehold.co/360x340?text=Hover+Image+3",
-      description: "Product 3",
-      price: "$39.99",
-    },
-    {
-      id: 4,
-      image: "https://placehold.co/360x340",
-      hoverImage: "https://placehold.co/360x340?text=Hover+Image+4",
-      description: "Product 4",
-      price: "$49.99",
-    },
-    {
-      id: 5,
-      image: "https://placehold.co/360x340",
-      hoverImage: "https://placehold.co/360x340?text=Hover+Image+5",
-      description: "Product 5",
-      price: "$59.99",
-    },
-    {
-      id: 6,
-      image: "https://placehold.co/360x340",
-      description: "Product 6",
-      price: "$49.99",
-    },
-    {
-      id: 7,
-      image: "https://placehold.co/360x340",
-      description: "Product 7",
-      price: "$59.99",
-    },
-    {
-      id: 8,
-      image: "https://placehold.co/360x340",
-      description: "Product 8",
-      price: "$49.99",
-    },
-    {
-      id: 9,
-      image: "https://placehold.co/360x340",
-      description: "Product 9",
-      price: "$59.99",
-    },
-  ];
+  // const products = [
+  //   {
+  //     id: 1,
+  //     image:
+  //       "https://dt-elektrix.myshopify.com/cdn/shop/products/Electro07.jpg?v=1666157970&width=360",
+  //     hoverImage: "https://placehold.co/360x340?text=Hover+Image+1",
+  //     description: "Product 1",
+  //     price: "$19.99",
+  //   },
+  //   {
+  //     id: 2,
+  //     image: "https://placehold.co/360x340",
+  //     hoverImage: "https://placehold.co/360x340?text=Hover+Image+2",
+  //     description: "Product 2",
+  //     price: "$29.99",
+  //   },
+  //   {
+  //     id: 3,
+  //     image: "https://placehold.co/360x340",
+  //     hoverImage: "https://placehold.co/360x340?text=Hover+Image+3",
+  //     description: "Product 3",
+  //     price: "$39.99",
+  //   },
+  //   {
+  //     id: 4,
+  //     image: "https://placehold.co/360x340",
+  //     hoverImage: "https://placehold.co/360x340?text=Hover+Image+4",
+  //     description: "Product 4",
+  //     price: "$49.99",
+  //   },
+  //   {
+  //     id: 5,
+  //     image: "https://placehold.co/360x340",
+  //     hoverImage: "https://placehold.co/360x340?text=Hover+Image+5",
+  //     description: "Product 5",
+  //     price: "$59.99",
+  //   },
+  //   {
+  //     id: 6,
+  //     image: "https://placehold.co/360x340",
+  //     description: "Product 6",
+  //     price: "$49.99",
+  //   },
+  //   {
+  //     id: 7,
+  //     image: "https://placehold.co/360x340",
+  //     description: "Product 7",
+  //     price: "$59.99",
+  //   },
+  //   {
+  //     id: 8,
+  //     image: "https://placehold.co/360x340",
+  //     description: "Product 8",
+  //     price: "$49.99",
+  //   },
+  //   {
+  //     id: 9,
+  //     image: "https://placehold.co/360x340",
+  //     description: "Product 9",
+  //     price: "$59.99",
+  //   },
+  // ];
+
+  useEffect(() => {
+    const fetchGetProducts = async () => {
+      try {
+        const response = await getBestSellingProducts();
+        setproducts(response.data);
+        console.log("Products Best selling", response.data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    fetchGetProducts();
+  }, []);
 
   const handlers = useSwipeable({
     onSwipedLeft: () => {
@@ -140,133 +156,134 @@ const BestSeller = () => {
           },
         }}
       >
-        {products.map((product) => (
-          <Link to={""}>
-            <Box
-              key={product.id}
-              sx={{
-                position: "relative",
-                display: "flex",
-                flexDirection: "column",
-                // alignItems: "center",
-                minWidth: "280px",
-                padding: "20px",
-                borderRadius: "20px",
-                backgroundColor: "#f5f5f5",
-                boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)",
-                transition: "all 0.3s ease",
-                cursor: "pointer",
-                ":hover": {
-                  boxShadow: "0 6px 18px rgba(0, 0, 0, 0.2)",
-                },
-              }}
-              onMouseEnter={() => setHoveredProductId(product.id)}
-              onMouseLeave={() => setHoveredProductId(null)}
-            >
+        {products &&
+          products.map((product) => (
+            <Link to={""}>
               <Box
+                key={product.id}
                 sx={{
                   position: "relative",
-                  width: "100%",
-                  maxWidth: "240px",
-                  borderRadius: "10px",
-                  overflow: "hidden",
+                  display: "flex",
+                  flexDirection: "column",
+                  // alignItems: "center",
+                  minWidth: "280px",
+                  padding: "20px",
+                  borderRadius: "20px",
+                  backgroundColor: "#f5f5f5",
+                  boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)",
+                  transition: "all 0.3s ease",
+                  cursor: "pointer",
+                  ":hover": {
+                    boxShadow: "0 6px 18px rgba(0, 0, 0, 0.2)",
+                  },
                 }}
+                onMouseEnter={() => setHoveredProductId(product.id)}
+                onMouseLeave={() => setHoveredProductId(null)}
               >
                 <Box
-                  component="img"
-                  src={product.image}
-                  alt={product.description}
                   sx={{
+                    position: "relative",
                     width: "100%",
-                    height: "auto",
-                    transition: "opacity 0.5s ease",
-                    opacity: hoveredProductId === product.id ? 0 : 1,
+                    maxWidth: "240px",
+                    borderRadius: "10px",
+                    overflow: "hidden",
                   }}
-                />
+                >
+                  <Box
+                    component="img"
+                    src={product.image}
+                    alt={product.description}
+                    sx={{
+                      width: "100%",
+                      height: "auto",
+                      transition: "opacity 0.5s ease",
+                      opacity: hoveredProductId === product.id ? 0 : 1,
+                    }}
+                  />
+                  <Box
+                    component="img"
+                    src={product.image}
+                    alt={product.description}
+                    sx={{
+                      position: "absolute",
+                      top: 0,
+                      left: 0,
+                      width: "100%",
+                      height: "auto",
+                      transition: "opacity 0.5s ease",
+                      opacity: hoveredProductId === product.id ? 1 : 0,
+                    }}
+                  />
+                </Box>
+
                 <Box
-                  component="img"
-                  src={product.hoverImage}
-                  alt={product.description}
+                  className="hover-icons"
                   sx={{
                     position: "absolute",
-                    top: 0,
-                    left: 0,
-                    width: "100%",
-                    height: "auto",
-                    transition: "opacity 0.5s ease",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                    display: "flex",
+                    gap: "8px",
                     opacity: hoveredProductId === product.id ? 1 : 0,
+                    visibility:
+                      hoveredProductId === product.id ? "visible" : "hidden",
+                    transition: "opacity 0.3s ease, visibility 0.3s ease",
                   }}
-                />
-              </Box>
-
-              <Box
-                className="hover-icons"
-                sx={{
-                  position: "absolute",
-                  top: "50%",
-                  left: "50%",
-                  transform: "translate(-50%, -50%)",
-                  display: "flex",
-                  gap: "8px",
-                  opacity: hoveredProductId === product.id ? 1 : 0,
-                  visibility:
-                    hoveredProductId === product.id ? "visible" : "hidden",
-                  transition: "opacity 0.3s ease, visibility 0.3s ease",
-                }}
-              >
-                {[
-                  { icon: <ShoppingCart />, id: "cart" },
-                  { icon: <Layers />, id: "layers" },
-                  { icon: <Favorite />, id: "favorite" },
-                  { icon: <Search />, id: "search" },
-                ].map((item) => (
-                  <IconButton
-                    key={item.id}
-                    sx={{
-                      backgroundColor: "#2189ff",
-                      color: "#fff",
-                      borderRadius: "10px",
-                      width: "40px",
-                      height: "40px",
-                      "&:hover": {
-                        backgroundColor: "#000",
-                      },
-                    }}
-                  >
-                    {item.icon}
-                  </IconButton>
-                ))}
-              </Box>
-              <Typography
-                variant="caption"
-                fontSize={"12px"}
-                color={"#bebebe"}
-                sx={{ letterSpacing: "1px", marginBottom: "3x" }}
-              >
-                PRODUCTS-VIBE
-              </Typography>
-              <Typography
-                variant="body1"
-                fontWeight="400"
-                // textAlign="center"
-                sx={{ marginBottom: "8px" }}
-              >
-                {product.description}
-              </Typography>
-              <Box
-                display={"flex"}
-                // alignItems="center"
-                justifyContent="space-between"
-                sx={{ marginTop: "auto" }}
-              >
-                <Typography variant="h6" fontSize={"22px"} fontWeight="600">
-                  {product.price}
+                >
+                  {[
+                    { icon: <ShoppingCart />, id: "cart" },
+                    { icon: <Layers />, id: "layers" },
+                    { icon: <Favorite />, id: "favorite" },
+                    { icon: <Search />, id: "search" },
+                  ].map((item) => (
+                    <IconButton
+                      key={item.id}
+                      sx={{
+                        backgroundColor: "#2189ff",
+                        color: "#fff",
+                        borderRadius: "10px",
+                        width: "40px",
+                        height: "40px",
+                        "&:hover": {
+                          backgroundColor: "#000",
+                        },
+                      }}
+                    >
+                      {item.icon}
+                    </IconButton>
+                  ))}
+                </Box>
+                <Typography
+                  variant="caption"
+                  fontSize={"12px"}
+                  color={"#bebebe"}
+                  sx={{ letterSpacing: "1px", marginBottom: "3x" }}
+                >
+                  {product.category_name}
                 </Typography>
-                <ChevronRight sx={{ color: "#2189ff", marginLeft: "8px" }} />
+                <Typography
+                  variant="body1"
+                  fontWeight="400"
+                  // textAlign="center"
+                  sx={{ marginBottom: "8px" }}
+                >
+                  {product.name.substr(0, 20)}
+                </Typography>
+                <Box
+                  display={"flex"}
+                  // alignItems="center"
+                  justifyContent="space-between"
+                  sx={{ marginTop: "auto" }}
+                >
+                  <Typography variant="h6" fontSize={"22px"} fontWeight="600">
+                    ${product.price}
+                  </Typography>
+                  <ChevronRight sx={{ color: "#2189ff", marginLeft: "8px" }} />
+                </Box>
               </Box>
-            </Box>
-          </Link>
-        ))}
+            </Link>
+          ))}
       </Box>
     </Box>
   );
